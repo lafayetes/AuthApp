e9949cdf973145513101354bfa0d546837515164
Diego La Faye <lafayediego@gmail.com.com>
2024-02-25 19:08:56 -0400

    modified app routing module with hash to not have problems on prod.  And changed url to the prod url

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -2,5 +2,5 @@
 
 
 export const environment = {
-  baseUrl:'http://localhost:3000'
+  baseUrl:'https://nest-auth-backend-dlf.up.railway.app' // Estamos agregando la url que obtuvimos al subir el backend a railway
 }
